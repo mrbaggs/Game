205b9f8f39874ced160a2aa033a56e95b6701f04
Scott Bagshaw <scott.bagshaw@baesystems.com>
2018-09-01 18:18:21 +0100

    added material ui

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,13 +1,16 @@
+import Button from "@material-ui/core/Button";
 const React = require("react");
 
 class Test extends React.Component {
     render() {
         return (
             <div>
-                This is my test div.
+                <Button variant="contained">
+                    {this.props.test}
+                </Button>
             </div>
         )
     }
 };
 
-module.exports = Test;
+export default Test;
